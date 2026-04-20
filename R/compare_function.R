a52135577dd4fe779ffe0e25000029df811f1095
t <>
2026-04-20 18:33:35 +0200

    new function

---
+++ R/compare_function.R
@@ -0,0 +1,67 @@
+library(ncdf4)
+library(metR)
+library(ggplot2)
+library(dplyr)
+library(tidyterra)
+
+
+#this function takes DGVM LAI data as input and compares it to LAI observations
+
+
+
+compare <- function(dgvm) {
+  #read data. LAI from one of the models.
+  LAI <- metR::ReadNetCDF(paste0("data/trendyv14_lai_july_mean/", dgvm, ".nc")) |>
+    as_tibble()
+  
+  #filter data from 1982 to 2022
+  LAI <- LAI |> dplyr::filter(time >= as.Date("1981-12-31"), time <= as.Date("2021-12-31"))
+  
+  #filter latitudes above 60 degrees
+  LAI <- LAI |> dplyr::filter("latitude" >= 60))
+  
+  
+  
+  #build multilayer spatraster with one layer per year
+  years_f <- sort(unique(LAI$time))
+  
+  raster_list_f <- lapply(years_f, function(yr) {
+    LAI |>
+      dplyr::filter(time == yr) |>
+      dplyr::select(longitude, latitude, lai) |>
+      terra::rast(type = "xyz", crs = "EPSG:4326")
+  })
+  
+  raster_LAI_f <- terra::rast(raster_list_f)
+  names(raster_LAI_f) <- years_f
+  
+  
+  #remove ocean surface cells
+  land <- terra::vect("data/spatial/land_surface/ne_10m_land.shp")
+  raster_LAI_f <- raster_LAI_f |> terra::mask(land)
+  
+  
+  #calculate Arctic mean LAI over time
+  arc_mean_f <- terra::global(raster_LAI_f, mean, na.rm = TRUE)
+  arc_mean_f <- arc_mean_f |> dplyr::mutate(year = 1982:2021) #add year column for plot
+  
+  #plot mean LAI from 1982-2022 with trendline
+  p_mean_LAI_f <- ggplot() +
+    geom_line(data = arc_mean_f,
+              aes(x = year, y = mean), color = "blue") +
+    geom_line(data = arctic_mean, aes(x = time, y = LAI)) + #observations
+    labs(title = paste0(dgvm, ": Arctic LAI 1982-2021")) 
+  p_mean_LAI_f
+  
+  
+  
+  #calculate deviation from observed mean
+  
+  
+  
+}
+
+
+
+
+
